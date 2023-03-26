6499e799c4f34242614006285aa1fed1f64319c2
Abraham Karikari <123241255+ABKarikari23@users.noreply.github.com>
2023-03-26 15:37:42 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -9,6 +9,7 @@
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
+ * By Abraham and Maryann
  */
 int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size)
